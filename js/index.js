33fbf2e7a6e9e588fceed4daf2efab5691b3cb77
max221088 <max.work221088@gmail.com>
2022-11-12 19:58:14 +0200

    add js first stage

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const $bottNext = document.querySelector('.arrow.next');
+const $bottPrev = document.querySelector('.arrow.prev');
+const $sliderRow = document.querySelector('.slider-row');
+let distance = 0;
+let currentSlide = 0;
+ console.log($bottNext);
+
+ function move (x) {
+distance = x * 800 * (-1);
+$sliderRow.style.transform = 'translateX(' + distance + 'px)';
+};
+
+$bottNext.addEventListener('click', function(){
+    currentSlide++;
+    console.log(currentSlide);
+    move (currentSlide);
+});
+
+$bottPrev.addEventListener('click', function(){
+    currentSlide--;
+    console.log(currentSlide);
+    move (currentSlide);
+});
